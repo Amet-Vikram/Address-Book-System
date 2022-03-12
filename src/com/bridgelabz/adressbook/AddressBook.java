cd255f6334eb5bd8eac54c3d3d4ec7575b15a679
Amet-Vikram <amet1197@gmail.com>
2022-03-12 11:24:20 +0530

    Feature to add new Contacts and display them.

--- src/com/bridgelabz/adressbook/AddressBook.java
+++ src/com/bridgelabz/adressbook/AddressBook.java
@@ -1,16 +1,41 @@
 package com.bridgelabz.adressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
+    int index = 0;
+    static Scanner sc = new Scanner(System.in);
     ArrayList<Contact> entry = new ArrayList<>();
 
-    void setContact(Contact o){
-        entry.add(o);
+    //contact obj
+    //add details here
+    void setContact(){
+        Contact person = new Contact(index);
+        System.out.println("Enter First Name: ");
+        person.setFirstName(sc.nextLine());
+        System.out.println("Enter Last Name: ");
+        person.setLastName(sc.nextLine());
+        System.out.println("Enter Address: ");
+        person.setAddress(sc.nextLine());
+        System.out.println("Enter City: ");
+        person.setCity(sc.nextLine());
+        System.out.println("Enter State: ");
+        person.setState(sc.nextLine());
+        System.out.println("Enter Zip Code: ");
+        person.setZip(sc.nextLine());
+        System.out.println("Enter Phone Number: ");
+        person.setPhoneNumber(sc.nextLine());
+        System.out.println("Enter E-Mail: ");
+        person.seteMail(sc.nextLine());
+        System.out.println("Add More Entry? ");
+        entry.add(person);
+        index ++;
     }
 
     void getContact(){
         for (Contact e: entry) {
+            System.out.println("ID: " + e.id);
             System.out.println("First Name: " + e.firstName);
             System.out.println("Last Name: " + e.lastName);
             System.out.println("Address: " + e.address);
@@ -18,11 +43,12 @@ public class AddressBook {
             System.out.println("State: " + e.state);
             System.out.println("Zip: " + e.zip);
             System.out.println("Phone Number: " + e.phoneNumber);
-            System.out.println("E-Mail: " + e.eMail);
+            System.out.println("E-Mail: " + e.eMail + "\n");
         }
     }
 }
 class Contact {
+    int id;
     String address;
     String firstName;
     String lastName;
@@ -31,6 +57,10 @@ class Contact {
     String zip;
     String phoneNumber;
     String eMail;
+
+    Contact(int index){
+        this.id = index;
+    }
 
     public void setAddress(String address) {
         this.address = address;
